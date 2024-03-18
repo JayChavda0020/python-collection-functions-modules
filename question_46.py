2e2506d2adf932cc6ba608972b398878c84db2b1
JayChavda0020 <jaychavdam0020@gmail.com>
2024-03-18 22:59:15 +0530

    18-03-2024

--- question_46.py
+++ question_46.py
@@ -2,3 +2,14 @@
 # Sample data: [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, o {'item': 'item1', 'amount': 750}]
 # Expected Output: Counter ({'item1': 1150, 'item2': 300}) 
 
+from collections import Counter
+
+data = [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
+
+result = Counter()
+
+for d in data:
+    result[d['item']] += d['amount']
+
+print(result)
+
